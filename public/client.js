fffe558e54508b3332f169f60e03e17562ebb3b6
tanweergit <technoafshaan@gmail.com>
2023-02-18 23:00:05 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,61 @@
+const socket = io();
+
+let name;
+let textarea = document.querySelector('#textarea');
+let meassageArea = document.querySelector('.message__area')
+
+do {
+  name = prompt('Please Enter Your Name: ')
+} while (!name)
+
+textarea.addEventListener('keyup', (e) => {
+  if(e.key === 'Enter'){
+    sendMessage(e.target.value)
+  }
+})
+
+function sendMessage(message){
+  let msg = {
+    user:name,
+    message:message.trim()
+  }
+//  Append
+  
+ appendMessage(msg,'outgoing')
+ textarea.value = ''
+ scrollToBottom()
+
+//  send to server
+
+ socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type){
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    meassageArea.appendChild(mainDiv)
+}
+
+
+// Recieve messages
+
+socket.on('message', (msg) => {
+  //  console.log(msg);
+  appendMessage(msg, 'incoming')
+  scrollToBottom()
+})
+
+// scroll
+
+function scrollToBottom() {
+  meassageArea.scrollTop = meassageArea.scrollHeight
+}
